02e72aeae076b9c4dc5baff4dce43014dfd5c4e9
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2022-11-27 00:33:01 -0500

    JS Applications - 02. Asynchronous Programming - exercises 04 - 06.

--- 02-Asynchronous Programming/05. Accordion/accordion.js
+++ 02-Asynchronous Programming/05. Accordion/accordion.js
@@ -1,3 +1,55 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    let mainListUrl = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    let response = await fetch(mainListUrl);
+    let mainList = await response.json();
+
+    let mainSection = document.getElementById('main');
+
+    mainList.forEach(item => createAccordion(item));
+
+    function createAccordion(item){
+        let divAccordion = document.createElement('div');
+        divAccordion.classList.add('accordion');
+
+        divAccordion.innerHTML = '<div class="head">\n' +
+                '<span></span>\n' +
+                '<button class="button" id="">More</button>\n' +
+                '</div>\n' +
+                '<div class="extra">\n' +
+                '<p></p>\n' +
+                '</div>';
+
+        let spanHead = divAccordion.querySelector('.head span');
+        spanHead.textContent = item.title;
+
+        let button = divAccordion.querySelector('.button');
+        button.id = item._id;
+
+        mainSection.appendChild(divAccordion);
+
+        button.addEventListener('click', (e) => {
+            let currentExtraParagraph = e.currentTarget.parentNode.parentNode.querySelector('.extra p');
+
+            if (e.target.textContent === 'More'){
+
+                if (currentExtraParagraph.textContent === ''){
+                    fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${e.target.id}`)
+                        .then(response => response.json())
+                        .then(element => {
+                            currentExtraParagraph.textContent = element.content;
+                        });
+                }
+
+                currentExtraParagraph.parentElement.style.display = 'inline-block';
+                e.currentTarget.textContent = 'Less';
+            }else{
+                currentExtraParagraph.parentElement.style.display = 'none';
+                e.currentTarget.textContent = 'More';
+            }
+        });
+    }
 }
+
+// window.addEventListener('load', solution);
+solution();
--- 02-Asynchronous Programming/06.Blog/app.js
+++ 02-Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,68 @@
-function attachEvents() {
-    console.log('TODO...');
+async function attachEvents() {
+    const loadPostsBtn = document.getElementById('btnLoadPosts');
+    const postsSelect = document.getElementById('posts');
+    let posts = {};
+
+    loadPostsBtn.addEventListener('click', async () => {
+        postsSelect.innerHTML = '';
+
+        const response = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        posts = await response.json();
+
+        // console.log(posts);
+
+        Object.keys(posts).forEach(post => {
+            const option = document.createElement('option');
+            option.textContent = posts[post].title;
+            option.value = posts[post].id;
+
+            postsSelect.appendChild(option);
+        });
+    });
+
+    const viewPostsBtn = document.getElementById('btnViewPost');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const ulPostComments = document.getElementById('post-comments');
+
+    const commentsResponse = await fetch(`http://localhost:3030/jsonstore/blog/comments`);
+    const comments = await commentsResponse.json();
+
+    const commentsArray = Object.entries(comments);
+
+
+    viewPostsBtn.addEventListener('click', async (e) => {
+        // console.log(postsSelect.value);
+        let postsArray = Object.entries(posts);
+
+
+        const currentPostId = postsSelect.value;
+
+        // const currentPost = posts.find(post => post.id === currentPostId);
+        // console.log(currentPost);
+
+        const currentPost = postsArray.find(post => post[0] === currentPostId);
+        // console.log(currentPost);
+        postTitle.textContent = currentPost[1].title;
+        postBody.textContent = currentPost[1].body;
+
+        // console.log(Object.entries(posts));
+        // console.log(postsArray);
+
+        let currentComments = commentsArray.filter(comment => comment[1].postId === currentPostId);
+
+        // console.log(currentComments);
+
+        ulPostComments.innerHTML = '';
+
+        currentComments.forEach(comment => {
+            const li = document.createElement('li');
+            li.textContent = comment[1].text;
+
+            ulPostComments.appendChild(li);
+        });
+    });
+
 }
 
 attachEvents();
